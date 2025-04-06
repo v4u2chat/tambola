bae560f45c8c8e0d721254353ba4ea40bbc0709c
Rajesh Gadiparthi <v4u2chat@gmail.com>
2025-04-06 12:34:57 +0530

    Update ClaimValidatorTest.java

--- src/test/java/com/sahaj/assignments/tambola/ClaimValidatorTest.java
+++ src/test/java/com/sahaj/assignments/tambola/ClaimValidatorTest.java
@@ -12,15 +12,13 @@ import static org.junit.Assert.assertEquals;
 public class ClaimValidatorTest extends TestCase {
     private ClaimValidator validator;
     private Ticket ticket;
-    private List<Integer> announcedNumbersAccepted; // From Example 1
-    private List<Integer> announcedNumbersRejected; // From Example 2
+    private List<Integer> announcedNumbersAccepted;
+    private List<Integer> announcedNumbersRejected;
 
     @Before
     public void setUp() {
-        // Initialize validator
         validator = new ClaimValidator();
 
-        // Same ticket from TambolaGame.java
         int[][] ticketNumbers = {
                 {4, 16, 0, 48, 63, 76, 0, 0, 0},
                 {7, 0, 23, 38, 0, 52, 0, 80, 0},
@@ -28,7 +26,6 @@ public class ClaimValidatorTest extends TestCase {
         };
         ticket = new Ticket(ticketNumbers);
 
-        // Same announced numbers from examples
         announcedNumbersAccepted = Arrays.asList(90, 4, 46, 63, 89, 16, 76, 48); // TR complete
         announcedNumbersRejected = Arrays.asList(90, 4, 46, 63, 89, 16, 76, 48, 12); // Late claim
     }
